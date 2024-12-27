b8bee9648efad8258f8ad0838491d4a70ac519bb
smcoderr <souravoz2018@gmail.com>
2024-12-27 14:14:50 +0530

    1

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/react-template-02/', // Ensure this matches your repository name
+  base: '/react-template-02/'
 });
